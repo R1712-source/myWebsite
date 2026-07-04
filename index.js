7b75ce2a4a1d2c34a932a1ac983567013ceeb91b
R1712-source <wasimhalabi33@gmail.com>
2026-07-04 10:24:34 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const introBtn = document.getElementById("introBtn");
 const detail = document.getElementById("detail");
+const box = document.getElementById("introDesc");
 
 function disp() {
   const isHidden = window.getComputedStyle(detail).display === 'none';
 
   detail.style.display = isHidden ? 'block' : 'none';
+  box.classList.toggle('blurred', isHidden);
 }
 
